62d156bd3e735b8b8742b47421fc4f37c8dd9d0f
Chris Thompson <chris.thompson@govdelivery.com>
2014-01-20 07:23:25 -0600

    add hint for test login

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
 account = Account.where(dcm_account_code: "ACME").first_or_create
-user = User.where(email: "test@te.st").first_or_initialize do |user|
-  user.password = user.password_confirmation = "testing1"
-end
-
+user = User.where(email: "test@te.st").first_or_initialize
+user.password = user.password_confirmation = "testing1"
 user.account = account
 user.save
