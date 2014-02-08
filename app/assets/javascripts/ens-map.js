94aa8ef6cc6a7797e633366e9792672c74031235
Chris Thompson <chris.thompson@govdelivery.com>
2014-02-07 19:16:32 -0600

    working editable map

--- app/assets/javascripts/ens-map.js
+++ app/assets/javascripts/ens-map.js
@@ -1,15 +1,56 @@
-if ($('#geojson-serialized')) {
-   console.log( $('#geojson-serialized').value);
+// if ($('#geojson-serialized')) {
+//    console.log( $('#geojson-serialized').value);
+// }
+
+geojsony = {
+    "properties": {},
+    "type": "Feature",
+    "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+            [
+                [
+                    175.2847623825073,
+                    -37.77495501866911
+                ],
+                [
+                    175.2933883666992,
+                    -37.77298755890846
+                ],
+                [
+                    175.29175758361816,
+                    -37.779398571318765
+                ],
+                [
+                    175.28845310211182,
+                    -37.78075532805877
+                ],
+                [
+                    175.2847623825073,
+                    -37.77495501866911
+                ]
+            ]
+        ]
+    }
 }
+
+
 
 var cloudmadeUrl = 'http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png'
 var cloudmade = new L.TileLayer(cloudmadeUrl, {maxZoom: 18});
+
+
+
+
+geo_layer = L.geoJson(geojsony);
+// geo_layer = L.GeoJson.geometryToLayer(geojsony); older version perhaps
+
 window.map = new L.Map('map', {layers: [cloudmade], center: new L.LatLng(-37.7772, 175.2756), zoom: 15 });
 
 L.Icon.Default.imagePath = '/assets';
 
-var editableLayers = new L.FeatureGroup();
-map.addLayer(editableLayers);
+var editableLayers = L.geoJson(geojsony).addTo(window.map);
+
 
 options = function(editableLayers) {
     return {
@@ -22,7 +63,7 @@ options = function(editableLayers) {
         // }
         // },
         //use default rectangle only
-        polygon: false,
+        // polygon: false,
         polyline: false,
         circle: false,
         marker: false
@@ -35,14 +76,25 @@ options = function(editableLayers) {
 var drawControl = new L.Control.Draw(options(editableLayers));
 map.addControl(drawControl);
 
+search_layer = new L.Control.GeoSearch({
+    provider: new L.GeoSearch.Provider.OpenStreetMap(),
+    position: 'bottomleft',
+    searchLabel: "City:",
+    zoomLevel:  10
+//    onRemove: function(){},
+}).addTo(map);
+
+
+map.addLayer(search_layer);
+
 function drawnItemHandler (layer){
     var geo_path = $('#map').data('geo-path');
     // var sse_channel = $('#map').data('sse-channel');
-    var really_json_really = build_json(layer)
+    var really_json = build_json(layer)
 
 
     $.post( geo_path,
-            {geo: {geojson: really_json_really}},
+            {geo: {geojson: really_json}},
              // sse_channel: sse_channel},
             function(response){
                 //message?
@@ -69,18 +121,7 @@ function build_json(layer) {
 
 map.on('draw:created', function(e){ drawnItemHandler( e.layer ); });
 map.on('draw:edited', function(e){ e.layers.eachLayer( drawnItemHandler ); });
-map.on('draw:deleted', function(e){ deletedItemHandler(e.layer) });
-
-search_layer = new L.Control.GeoSearch({
-    provider: new L.GeoSearch.Provider.OpenStreetMap(),
-    position: 'bottomleft',
-    searchLabel: "City:",
-    zoomLevel:  10
-//    onRemove: function(){},
-}).addTo(map);
-
-
-map.addLayer(search_layer);
+// map.on('draw:deleted', function(e){ deletedItemHandler(e.layer) });
 
 
                 // drawControl.removeFrom(map);
