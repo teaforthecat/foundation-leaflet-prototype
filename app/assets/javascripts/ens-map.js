4e5243c41b62e8ef770c7516ab3bae4254656d4f
Chris Thompson <chris.thompson@govdelivery.com>
2014-02-15 13:44:27 -0600

    new map awesomness

--- app/assets/javascripts/ens-map.js
+++ app/assets/javascripts/ens-map.js
@@ -3,8 +3,9 @@ var cloudmade = new L.TileLayer(cloudmadeUrl, {maxZoom: 15});
 
 // var center = L.latLngBounds(southWest, northEast).getCenter();
 window.map = new L.Map('map', {layers: [cloudmade],
+                               // TODO: use current location for default latLng
                                center: new L.LatLng(-37.7772, 175.2756),
-                               zoom: 15 });
+                               zoom: 10 });
 
 L.Icon.Default.imagePath = '/assets';
 
@@ -12,8 +13,20 @@ L.Icon.Default.imagePath = '/assets';
 if ( $('#currentGeoObject').data( 'geo' ) ) {
     geojsony = $('#currentGeoObject').data( 'geo' );
     var editableLayers = L.geoJson(geojsony).addTo(window.map);
+    window.searchValue = geojsony.properties.search_value || "City: ";
+    var mapBounds = geojsony.properties.map_bounds;
+    var sw_lat = parseFloat(mapBounds._southWest.lat);
+    var sw_lng = parseFloat(mapBounds._southWest.lng);
+    var ne_lat = parseFloat(mapBounds._northEast.lat);
+    var ne_lng = parseFloat(mapBounds._northEast.lng);
+
+    window.map.fitBounds([
+        [sw_lat, sw_lng],
+        [ne_lat, ne_lng]
+    ]);
 } else {
     var editableLayers = new L.FeatureGroup();
+    window.searchValue = "City: ";
 }
 
 
@@ -21,21 +34,23 @@ options = function(editableLayers) {
     return {
         position: 'topright',
         draw: {
-
+            //use default rectangle only
             // rectangle: {
             //     shape_options: {
             //     color: "something"
-            // }
+            //     },
             // },
-            //use default rectangle only
-            // polygon: false,
+            polygon: false,
             polyline: false,
             circle: false,
             marker: false
         },
         edit: {
             featureGroup: editableLayers, //REQUIRED!!
-            remove: true
+            //layers are cleared when a shape is drawn
+            // and a map should not be valid without a shape
+            // so don't allow to delete a shape
+            remove: false
         }
     };
 };
@@ -46,8 +61,9 @@ map.addControl(drawControl);
 search_layer = new L.Control.GeoSearch({
     provider: new L.GeoSearch.Provider.OpenStreetMap(),
     position: 'bottomleft',
-    searchLabel: "City:",
+    searchLabel: window.searchValue, // set to saved or default value above
     zoomLevel:  10
+
     //    onRemove: function(){},
 }).addTo(map);
 
@@ -82,20 +98,27 @@ function drawnItemHandler (layer){
 
 function build_json(layer) {
     // GeoSearch plugin:
-    var search_value = $('#leaflet-control-geosearch-qry').value;
+    // val() will exist only if it was edited
+    var search_value = $('#leaflet-control-geosearch-qry').val();
+    // the placeholder is set by the plugin
+    // we need to store it on the page or it will get overwritten when saving the geojson
+    placeholder = $('#leaflet-control-geosearch-qry').attr('placeholder');
+    // don't save the default placeholder
+    real_search_value = search_value || ((placeholder == 'City: ') ? "" : placeholder );
+
     //global map
     var map_bounds = map.getBounds();
     var zoom = map.getZoom();
     var geoJson = layer.toGeoJSON();
-    console.log('geo out')
-    console.log(geoJson.geometry);
+    // console.log('geo out')
+    // console.log(geoJson.geometry);
     var geo = $.extend(true, {}, geoJson,
-                       { properties: { search_value: search_value,
-                                       map_bounds: map_bounds,
-                                       zoom: zoom}});
+                       { properties: { search_value: real_search_value,
+                                       map_bounds: map_bounds}});
 
     // TODO: the coordinates array seems to be converted into an object
-    // in the form_vars of the request
+    // in the form_vars of the request, it would be better if the coordinates
+    // were kept in an array of arrays
     // purge functions
     return JSON.parse(JSON.stringify(geo));
 }
@@ -104,5 +127,15 @@ map.on('draw:created', function(e){
     drawnItemHandler( e.layer );
 });
 
-// map.on('draw:edited', function(e){ e.layers.eachLayer( drawnItemHandler ); });
-// map.on('draw:deleted', function(e){ deletedItemHandler(e.layer) });
+
+// only one shape allowed
+map.on('draw:drawstart', function(e){
+    editableLayers.clearLayers();
+})
+
+map.on('draw:edited', function(e){
+    // we have the luxury of using the first editableLayer
+    // because we limit the number of layers to one by clearLayers
+    // in drawstart
+    drawnItemHandler( editableLayers.getLayers()[0] );
+});
