64d309e05a79163997019d47975d636f1512e6ce
Chris Thompson <chris.thompson@govdelivery.com>
2014-02-24 10:26:06 -0600

    turn off turbolinks for now

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,28 +1,22 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+// require turbolinks // having some trouble with foundation
 //= require foundation
 //= require ckeditor-jquery
 //= require eventsource
 //= require notification
 
-// this works with turbolinks
-window.onload = function() {
-        $(document).foundation();
-};
 
+// NOTE: other init strategies break foundation/turbolinks integration
+// $(document).on('ready',
+//                function(){
+//                    $(document).foundation();
+//                }
+// );
+
+$(function(){
+        $(document).foundation();
+});
 
 $('.ckeditor').ckeditor({
   // optional config
